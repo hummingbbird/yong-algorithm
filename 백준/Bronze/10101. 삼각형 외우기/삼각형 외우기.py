11e87e05f82c8356b98937e9de972819416c4ac6
hummingbbird <84303489+hummingbbird@users.noreply.github.com>
2024-06-24 00:23:19 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -1,11 +1,13 @@
-angle=[]
+lst = []
 for _ in range(3):
-    angle.append(int(input()))
-if sum(angle)!=180:
-    print("Error")
-elif angle[0]==60 and angle[1]==60 and angle[2]==60:
-    print("Equilateral")
-elif (angle[0]==angle[1]) or (angle[2]==angle[1]) or (angle[0]==angle[2]):
-    print("Isosceles")
+  lst.append(int(input()))
+
+
+if sum(lst) != 180:
+  print("Error")
+elif len(set(lst)) == 1:
+  print("Equilateral")
+elif len(set(lst)) == 2:
+  print("Isosceles")
 else:
-    print("Scalene")
+  print("Scalene")
