0444e00cf2cd71460ea32343ba0f45359148c5f3
hummingbbird <84303489+hummingbbird@users.noreply.github.com>
2024-07-05 15:18:04 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,4 +1,16 @@
 a = int(input())
-ans = [0,1,2,4,7,13,24,44,81,149,274]
+# ans = [0,1,2,4,7,13,24,44,81,149,274]
+
 for _ in range(a):
-  print(ans[int(input())])
+  n = int(input())
+  if n == 1 or n == 2:
+    print(n)
+    continue
+  #3부터 여기를 온다는 뜻
+  cases = [0 for i in range(n+1)]
+  cases[0] = 1
+  cases[1] = 1
+  cases[2] = 2
+  for i in range(3, n+1):
+    cases[i] = cases[i-1] + cases[i-2] + cases[i-3]
+  print(cases[n])
