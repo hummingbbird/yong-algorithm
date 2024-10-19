1c51137bf9efe0dd4cb2b4317d53c6fcbba9ee8d
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2024-10-19 21:34:40 +0900

    [Gold V] Title: LCS, Time: 476 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,24 +1,24 @@
 import sys
 sys.setrecursionlimit(10**7)
+
 A = input()
 B = input()
+m, n = len(B), len(A)
+dp = [[0] * m for _ in range(n)]
 
-def LCS(X, Y):
-  m, n = len(A), len(B)
-  dp = [[-1] * n for _ in range(m)]
-
-  def S(i, j):
-    if i<0 or j<0: 
-      return 0
-    if dp[i][j] != -1: return dp[i][j]
-
-    if X[i] == Y[j]:
-      dp[i][j] = S(i-1, j-1)+1
+for i in range(n):
+  for j in range(m):
+    if i==0 and j==0 and A[i]==B[j]:
+      dp[0][0]=1
+    if A[i] == B[j]:
+      # 테두리일 경우 1로 설정(아닌 건 0이니까)
+      if i == 0 or j==0:
+        dp[i][j] = 1
+      # 둘이 같은데 테두리 아니야
+      else:
+        dp[i][j]=dp[i-1][j-1]+1
+    # 둘이 달라
     else:
-      dp[i][j] = max(S(i, j-1), S(i-1, j))
+      dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
-    return dp[i][j]
-
-  return S(m-1, n-1)
-print(LCS(A, B))
-
+print(dp[n-1][m-1])
