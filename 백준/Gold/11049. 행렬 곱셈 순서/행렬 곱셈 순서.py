14dc5a5a5a344c6c9b317e31c13bf78208b4e5c2
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2024-10-20 16:30:45 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 536 ms, Memory: 112844 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,11 +1,25 @@
-n = int(input())
-matrix = [tuple(map(int, input().split())) for i in range(n)]
+import math
 
-dp = [[0]*n for i in range(n)]
-for count in range(n-1):
-    for i in range(n-1-count):
-        j = i+count+1
-        dp[i][j] = 2**31
-        for k in range(i, j):
-            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + matrix[i][0]*matrix[k][1]*matrix[j][1])
-print(dp[0][-1])
+def mcm(p):
+    n = len(p)
+    dp = [[math.inf] * n for _ in range(n)]
+    for s in range(n - 1):
+        dp[s][s + 1] = 0
+
+    for r in range(2, n):
+        for s in range(n - r):  # 시작 index
+            e = s + r  # 끝 index
+            for i in range(s + 1, e):
+                dp[s][e] = min(dp[s][e], dp[s][i] + dp[i][e] + p[s] * p[i] * p[e])
+
+    return dp[0][n - 1]
+
+n = int(input())
+matrix = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    matrix.append(a)
+    if i == n - 1:
+        matrix.append(b)
+print(mcm(matrix))
