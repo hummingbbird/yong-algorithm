10d409f308f64bd255416b8b397d6af88641301b
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2024-12-02 11:03:16 +0900

    [Gold III] Title: ACM Craft, Time: 1200 ms, Memory: 34472 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/1005
+# topological + dp
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -8,29 +9,30 @@ for _ in range(T):
   n, k = map(int, input().split(" "))
   delay=[0]+list(map(int, input().split(" "))) # 각 node별 짓는 데 걸리는 시간
   adj = [[] for _ in range(n+1)]
-  indegree = [0 for _ in range(n+1)]
+  d = [0 for _ in range(n+1)]
   dp = [0 for _ in range(n+1)]
 
   # 간선 입력
   for i in range(k):
     u, v = map(int, input().split(" "))
     adj[u].append(v)
-    indegree[v]+=1
+    d[v]+=1
     
-  que = deque()
+  S = deque()
   for i in range(1, n+1):
     # in-edges가 없다면 que에 추가하고, dp값 업데이트
-    if indegree[i] == 0:
-      que.append(i)
+    if d[i] == 0:
+      S.append(i)
       dp[i] = delay[i]
 
-  while que:
-    tmp=que.popleft()
+  while S:
+    tmp=S.popleft()
     for i in adj[tmp]:
-      indegree[i]-=1
+      d[i]-=1
       dp[i] = max(dp[tmp]+delay[i], dp[i])
-      if indegree[i] == 0:
-        que.append(i)
+      if d[i] == 0:
+        S.append(i)
 
   w=int(input())
   print(dp[w])
+
