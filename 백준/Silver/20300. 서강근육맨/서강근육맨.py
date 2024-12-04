e1858ad659cb61427aaeaaa335ac3625dc237dd8
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2024-12-04 19:45:56 +0900

    [Silver III] Title: 서강근육맨, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/20300. 서강근육맨/서강근육맨.py
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.py
@@ -1,4 +1,3 @@
-# https://www.acmicpc.net/problem/20300
 import sys
 input = sys.stdin.readline
 
@@ -6,21 +5,15 @@ n = int(input())
 equips = list(map(int, input().split()))
 equips.sort()
 
-
-# 짝수일 경우, 최대인 아이는 무조건 최소와 더해져야만 m의 크기를 최대한 줄일 수 있어
 if n%2==0:
-  m = -1
-  for i in range((n//2)):
-    tmp = equips[i] + equips[n-i-1]
-    if tmp > m:
-      m = tmp
+  m=-1
 else:
-  m = equips.pop()
-  for i in range((n//2)):
-    tmp = equips[i] + equips[n-i-2]
-    if tmp > m:
-      m = tmp
-  
-print(m)
-  
+  m=equips.pop()
+  n-=1
 
+for i in range(len(equips)//2):
+  tmp = equips[i] + equips[n-i-1]
+  if tmp > m:
+    m = tmp
+
+print(m)
