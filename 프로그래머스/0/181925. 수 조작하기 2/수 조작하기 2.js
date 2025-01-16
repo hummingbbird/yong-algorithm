18602459c2e5a7be34b269d5ef367b844c4b1d03
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-01-16 17:31:00 +0900

    [level 0] Title: 수 조작하기 2, Time: 35.18 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,15 @@
+
+function solution(numLog) {
+    var answer = '';
+    // 수의 변화량을 저장하는 리스트 사용하거나 빼가면서 쓰기
+    for (let i=1; i<numLog.length;i++){
+        const change = numLog[i] - numLog[i-1];
+        switch (change) {
+            case 1: answer += 'w'; break;
+            case -1: answer += 's'; break;
+            case 10: answer += 'd'; break;
+            case -10: answer += 'a'; break;
+        }
+    }
+    return answer;
+}
