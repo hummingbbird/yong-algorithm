748610133a5cf19af592e00d233c4a16afa55fe6
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-01-16 17:36:21 +0900

    add: 다른 풀이법 추가

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,15 +1,25 @@
+// 1. switch문을 사용한 구현
+// function solution1(numLog) {
+//     var answer = '';
+//     // 수의 변화량을 저장하는 리스트 사용하거나 빼가면서 쓰기
+//     for (let i=1; i<numLog.length;i++){
+//         const change = numLog[i] - numLog[i-1];
+//         switch (change) {
+//             case 1: answer += 'w'; break;
+//             case -1: answer += 's'; break;
+//             case 10: answer += 'd'; break;
+//             case -10: answer += 'a'; break;
+//         }
+//     }
+//     return answer;
+// }
 
+// 2. 객체 배열 만들어서 구현
+const convert = { '1': 'w', '-1':'s', '10':'d','-10':'a'};
 function solution(numLog) {
-    var answer = '';
-    // 수의 변화량을 저장하는 리스트 사용하거나 빼가면서 쓰기
+        var answer = '';
     for (let i=1; i<numLog.length;i++){
-        const change = numLog[i] - numLog[i-1];
-        switch (change) {
-            case 1: answer += 'w'; break;
-            case -1: answer += 's'; break;
-            case 10: answer += 'd'; break;
-            case -10: answer += 'a'; break;
-        }
+        answer += convert[numLog[i] - numLog[i-1]]
     }
     return answer;
 }
