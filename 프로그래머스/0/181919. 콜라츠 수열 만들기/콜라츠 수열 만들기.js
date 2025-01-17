f5239523e262fcf239f030031d7875769ebc7e73
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-01-17 09:29:56 +0900

    update: 조건문 삼항연산자로 변경

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -3,11 +3,7 @@ function solution(n) {
     var num = n;
     while (num > 1) {
         answer.push(num);
-        if (num % 2==0) {
-            num /= 2;
-        } else {
-            num = (3*num)+1;
-        }
+        num = num%2==0? (num/2):((3*num)+1);
     }
     answer.push(1);
     return answer;
