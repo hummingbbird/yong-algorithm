f24a722bd5bac0dbde2cec50b9e69a5a6f0fad37
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-01-24 16:46:42 +0900

    update: 풀이 방법 추가(spread 연산자 활용)

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -4,4 +4,8 @@ function solution(arr, intervals) {
         answer=answer.concat([...arr.slice(interval[0],interval[1]+1)]);
     })
     return answer;
+    
+    // 다른 사람의 풀이(better)
+    const [[a,b],[c,d]] = intervals; // 일케하면 각 원소가 a,b,c,d에 저장됨
+    return [...arr.slice(a,b+1), ...arr.slice(c, d+1)]; // spread 연산자 활용
 }
