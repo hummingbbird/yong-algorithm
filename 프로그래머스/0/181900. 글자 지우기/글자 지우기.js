b9ce37e8238ae95f14a0a46fb6b6e99cf4b8999b
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-01-24 11:15:38 +0900

    update: filter 메소드를 이용한 리팩토링

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,9 +1,10 @@
 function solution(my_string, indices) {
-    var answer = '';
-    [...my_string].map((it, i)=> {
-        if (!(indices.includes(i))) {
-            answer += it;
-        } 
-    })
-    return answer;
+    // var answer = '';
+    // [...my_string].map((it, i)=> {
+    //     if (!(indices.includes(i))) {
+    //         answer += it;
+    //     } 
+    // })
+    // return answer;
+    return [...my_string].filter((it, i)=>!(indices.includes(i))).join(""); // 한 줄로 리팩토링 ~
 }
