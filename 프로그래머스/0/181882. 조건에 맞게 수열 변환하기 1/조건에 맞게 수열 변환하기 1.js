2b1bb570a63cce60f870449963bc94fe108ab3b5
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-02-03 19:55:07 +0900

    refactor: 가독성을 위한 리펙토링 진행

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,9 +1,7 @@
 function solution(arr) {
     return arr.map((it) => {
-        if (it >= 50 && it%2===0) {
-            return it/2;
-        } else if (it < 50 && it%2!==0) {
-            return it*2;
-        } else return it;
+        if (it >= 50 && it%2===0) return it/2;
+        if (it < 50 && it%2!==0) return it*2;
+        return it;
     });
 }
