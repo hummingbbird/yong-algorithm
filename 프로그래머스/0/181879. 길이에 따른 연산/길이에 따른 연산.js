4d23fb76c41563754cc4267af16fdd08f1a8a517
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-02-04 13:44:53 +0900

    update: 개선된 코드 추가

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -2,3 +2,9 @@ function solution(num_list) {
     if (num_list.length>=11) return num_list.reduce((accu, item)=> accu+item, 0);
     else return num_list.reduce((accu, item)=> accu*item, 1);
 }
+
+// 조건문을 삼항 연산자로 바꿔 reduce문 안으로 넣어줄 수도 있음
+// 코드가 더 간결해짐
+function solution2(num_list) {
+    return num_list.reduce((a, e)=> num_list.length>10? a+e:a*e);
+}
