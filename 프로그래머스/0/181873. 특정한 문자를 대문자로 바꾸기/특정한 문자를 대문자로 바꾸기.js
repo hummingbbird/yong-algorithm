23bb5af8ec7a417e563157c8b19974ad1ca91db0
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-02-11 11:48:51 +0900

    update: replaceAll 메소드를 통한 코드 개선

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,3 +1,7 @@
 function solution(my_string, alp) {
+    // 1. map을 이용한 변환
     return [...my_string].map(it=>it===alp?it.toUpperCase():it.toLowerCase()).join("");
+
+    // 2. replaceAll 메소드를 이용한 변환(so easy!!!)
+    return my_string.replaceAll(alp, alp.toUpperCase());
 }
