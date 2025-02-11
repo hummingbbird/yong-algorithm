0c073f37888e947969b0cb04e84a143561a8d531
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-02-11 11:41:32 +0900

    update: 코드 개선

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
@@ -1,5 +1,3 @@
 function solution(strArr) {
-    return strArr.map((it,i)=>{
-        return (i%2==0)? it.toLowerCase():it.toUpperCase();
-    });
+    return strArr.map((it,i)=>(i%2==0)? it.toLowerCase():it.toUpperCase());
 }
