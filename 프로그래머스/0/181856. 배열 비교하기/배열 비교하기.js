c747aca8831893bb7503bf08c72a63768f4a8962
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-03-12 16:13:02 +0900

    refactor: 코드 개선

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,13 +1,13 @@
-const isSame = (a1, a2) => {
-    if (a1 === a2) return 0;
-    else if (a1 < a2) return -1;
-    else return 1;
+// option 인자 추가해서 두 배열의 길이가 같은 경우에도 이 함수를 사용할 수 있도록 함!
+// 비교 연산 코드의 중복을 줄일 수 있음.
+const isSame = (a1, a2, option) => {
+    if (option === "reduce") {
+        a1 = a1.reduce((accu, item)=>accu+item);
+        a2 = a2.reduce((accu, item)=>accu+item);        
+    }
+    return a1 > a2 ? 1 : a1<a2 ? -1 : 0;
 }
 
 function solution(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return arr1.length > arr2.length?1:-1;
-    } else {
-        return isSame(arr1.reduce((accu, item)=>accu+=item), arr2.reduce((accu, item)=>accu+=item));
-    }
+    return arr1.length!==arr2.length? isSame(arr1.length, arr2.length):isSame(arr1, arr2, "reduce");
 }
