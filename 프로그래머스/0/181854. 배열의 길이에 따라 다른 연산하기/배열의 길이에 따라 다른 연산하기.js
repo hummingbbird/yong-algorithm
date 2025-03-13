ea15a92284509f356c9008b4fc456ff2072d4893
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-03-14 02:24:24 +0900

    update: 다른 풀이 추가

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -7,3 +7,17 @@ function solution(arr, n) {
         }
     })
 }
+
+const solution2 = (arr, n) => arr.map((it, i)=> (
+        arr.length%2 !== i%2? it+n:it
+    ))
+
+function solution3 (arr, n) {
+    return arr.map((it, i)=> {
+        if (arr.length % 2 !== 0) {
+            return i%2 === 0? it+n: it;
+        } else {
+            return i%2 !== 0? it+n: it;
+        }
+    })
+}
