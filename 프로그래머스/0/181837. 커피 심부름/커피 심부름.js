5ddc80fa37d194b1d77bfe9de757af88e3f4d4fe
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-02 15:49:06 +0900

    update: 삼항연산자로 리팩토링

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,7 +1,9 @@
-function solution(order) {
-    return order.reduce((accu, it)=> {
-        if (it.indexOf('cafelatte') !== -1) {
-            return accu + 5000;
-        } else return accu + 4500;
-    },0)
-}
+// function solution(order) {
+//     return order.reduce((accu, it)=> {
+//         if (it.indexOf('cafelatte') !== -1) {
+//             return accu + 5000;
+//         } else return accu + 4500;
+//     },0)
+// }
+
+const solution = (order) => order.reduce((accu, it) => accu + (it.includes('latte')? 5000: 4500),0);
