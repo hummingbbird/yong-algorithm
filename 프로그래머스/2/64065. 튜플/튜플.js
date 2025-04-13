3c7d0dea193f4d64d9b5d7b22630e7b32dff2980
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-13 16:33:27 +0900

    refactor: 리팩토링 코드 추가

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,19 +1,33 @@
 function solution(s) {
     var answer = [];
     // 1. s를 리스트 형태로 변환
-    var ss = s.slice(2, s.length-2).split("},{").map(it=> {
+    var tuple = s.slice(2, s.length-2).split("},{").map(it=> {
         return it.split(",").map(it => parseInt(it));
     });
     
     // 2. 배열 길이 기준 오름차순 정렬
-    ss.sort(function(a,b){
+    tuple.sort(function(a,b){
         return a.length-b.length;
     });
     
     // 3. 완성하기
-    ss.forEach((it, i)=> {
+    tuple.forEach((it, i)=> {
         answer.push(it.filter(ele => !answer.includes(ele))[0]);
     })
+    
+    return answer;
+}
+
+// 리팩토링한 코드 .. 지만? 난 기존 게 더 가독성이 좋다고 생각 .. ~~
+function solution2(s) {
+    var answer = [];
+    
+    var tuple = s.slice(2, s.length-2).split("},{")
+    .map(it=> it.split(",").map(it => parseInt(it)))
+    .sort(function(a,b){return a.length-b.length;})
+    .forEach((it, i)=> {
+        answer.push(it.filter(ele => !answer.includes(ele))[0]);
+    });
     
     return answer;
 }
