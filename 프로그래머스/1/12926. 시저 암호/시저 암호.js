69e0f019b893f02a2086ecffa0779c85f390987f
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-15 15:50:21 +0900

    refactor: 정답 코드 리팩토링

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -13,3 +13,20 @@ function solution(s, n) {
     
     return answer;
 }
+
+// 리팩토링 후 코드
+function solution2(s, n) {
+    var answer = "";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijklmnopqrstuvwxyz"; // 굳이 toLowerCase 쓰지 말고 한 눈에 들어오게
+    for (const it of [...s]) {
+        if (it === " ") {
+            answer+=" ";
+            continue;
+        }
+        const charArr = upper.includes(it)? upper: lower; // 대소문자 구분을 먼저 진행해서 중복 코드 줄이기
+        answer+=charArr[((charArr.indexOf(it)+n)%26)];
+    }
+    
+    return answer;
+}
