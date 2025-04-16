ebb09bcd3670926c442d7500a6ccf5d2153061c7
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-16 16:38:55 +0900

    [level 2] Title: 귤 고르기, Time: 653.81 ms, Memory: 195 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,23 @@
+function solution(k, tangerine) {
+    // 1. 귤 크기별 개수 정리
+    var tanCounts = new Array(Math.max(...tangerine)+1).fill(0);
+    tangerine.forEach(it => tanCounts[it-1]+=1);
+    tanCounts = tanCounts.sort((a,b)=> b-a).filter(it => it !== 0);
+    
+    // 2. 정답 도출
+    var gyul = 0; // 귤 개수
+    var answer = 0; // 종류 개수
+    
+    for (let i=0;i<tanCounts.length;i++) {
+        if (gyul+tanCounts[i] === k) {
+            gyul += tanCounts[i];
+            answer += 1;
+        }
+        else {
+            gyul += tanCounts[i];
+            answer += 1;
+        }
+        if (gyul >= k) break;
+    }
+    return answer;
+}
