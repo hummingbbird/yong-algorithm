be10ca5feb4125cdc92726b3030a084c88fa3c09
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-23 16:28:33 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,7 @@
 function solution(clothes) {
     var answer = 1;
     var category = {};
-    clothes.forEach(it => {
-        if(!Object.keys(category).includes(it[1])) {
-            category[it[1]] = 1;
-        } else {
-            category[it[1]] += 1;
-        }
-    });
+    clothes.forEach(it => category[it[1]] = (category[it[1]]||0)+1);
     Object.values(category).forEach(it => answer *= (it+1));
     return answer-1;
 }
