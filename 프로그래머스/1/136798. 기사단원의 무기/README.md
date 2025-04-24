62b986406d83345165429dd1689befc117897b40
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-04-24 15:05:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 184.55 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 210.20 ms
+메모리: 36 MB, 시간: 184.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 15:03:27
+2025년 04월 24일 15:05:40
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,4 +1,3 @@
-// 기본적인데.. 무조건 시간초과남!!!
 const getYaksuCount = (n)=> {
     let cnt = 0;
     for(let i=1;i<=Math.sqrt(n);i++) {
@@ -10,9 +9,8 @@ const getYaksuCount = (n)=> {
 
 function solution(number, limit, power) {
     var answer = 0;
-    var yaksu = [];
     for(let i=1;i<=number;i++) {
-        yaksu.push(getYaksuCount(i)>limit?power:getYaksuCount(i));
+        answer+=(getYaksuCount(i)>limit?power:getYaksuCount(i));
     };
-    return yaksu.reduce((accu,item) => accu+item,0);
+    return answer;
 }
