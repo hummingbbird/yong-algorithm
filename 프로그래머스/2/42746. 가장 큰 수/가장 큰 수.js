a7721cdf0de460e1c86f37c654fabc652fe3c032
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-05-27 14:29:58 +0900

    [level 2] Title: 가장 큰 수, Time: 319.73 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,10 +1,13 @@
 function solution(numbers) {
+    // 모두 0인 경우 "0" 리턴
     if (numbers.reduce((prev, accu)=> accu+prev,0)===0) return "0";
+    
+    // 그 외의 경우 자릿수 맞추기
     numbers.sort(function(a,b){
-        if (parseInt(b.toString().repeat(4).slice(0,4)) === parseInt(a.toString().repeat(4).slice(0,4))) return b-a;
-        else {
-            return parseInt(b.toString().repeat(4).slice(0,4)) - parseInt(a.toString().repeat(4).slice(0,4));
-        }
+        const paddedB = parseInt(b.toString().repeat(4).slice(0,4))
+        const paddedA = parseInt(a.toString().repeat(4).slice(0,4));
+        if (paddedB===paddedA) return b-a;
+        else return paddedB - paddedA;
     });
     return numbers.map(it => it.toString()).join("");
 }
