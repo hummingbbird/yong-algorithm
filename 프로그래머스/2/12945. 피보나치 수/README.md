aee7a8c227fa1dc8e608a7ba2a5f43da71eb08a0
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-09-16 16:39:43 +0900

    [level 2] Title: 피보나치 수, Time: 512.68 ms, Memory: 455 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=python3) 
 
 ### 성능 요약
 
-메모리: 439 MB, 시간: 490.54 ms
+메모리: 455 MB, 시간: 512.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 11:55:23
+2025년 09월 16일 16:39:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,10 @@
-
 def solution(n):
-    numbers = [0 for i in range(n+1)]
-    for i in range(1, n+1):
-        if i==1:
-            numbers[i]=1
+    ans = [0 for i in range(n+1)]
+    for i in range(n+1):
+        if  i == 0 or i == 1:
+            ans[i] = i
         else:
-            numbers[i] = numbers[i-1]+numbers[i-2]
-    return numbers[-1]%1234567
+            ans[i] = ans[i-1] + ans[i-2]
+    return ans[-1] % 1234567
+    
+    
