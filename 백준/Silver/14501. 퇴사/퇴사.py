bd3cab4c63e6542141f50e67b60dd58881296285
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-09-24 01:14:00 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,31 +1,25 @@
 N = int(input())
-tp = []
-for _ in range(N):
-    tp.append(list(map(int, input().split())))
+T = [0] * N
+P = [0] * N
+for i in range(N):
+	T[i], P[i] = map(int, input().split())
+    
 
-# 방법1: 완전 탐색(dfs)
 def dfs(n, sm):
     global ans
-    # 1. 종료 조건
+    # 1. 종료 조건: n이 N 이상인 경우
     if n >= N:
         ans = max(ans, sm)
         return
+        
     # 2. 하부 호출 
     # case1) 상담하는 경우
-    if n + tp[n][0] <= N:
-        dfs(n+tp[n][0], sm+tp[n][1])
-    # case2) 상담 안 하는 경우
+    if n + T[n] <= N: # 날짜가 넘치지 않는 경우에만 상담 가능
+        dfs(n+T[n], sm+P[n])
+        
+    # case2) 상담 안 하는 경우: n만 1만큼 전진(총 이익 sm은 그대로)
     dfs(n+1, sm)
 
 ans = 0
 dfs(0,0)
 print(ans)
-
-# 방법2: dp
-dp = [0 for _ in range(N+1)]
-for n in range(N-1, -1, -1):
-    if n + tp[n][0] > N:
-        dp[n] = dp[n+1]
-    else:
-        dp[n] = max(dp[n+1], tp[n][1]+dp[n+tp[n][0]])
-# print(dp[0])
