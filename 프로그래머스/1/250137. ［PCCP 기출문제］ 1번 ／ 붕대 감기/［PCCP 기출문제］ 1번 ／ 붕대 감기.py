dd3a8523f712f732e5933337aebf86b5776a368a
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2026-09-01 14:53:40 +0900

    refactor: 리팩토링

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,33 +1,32 @@
 def solution(bandage, health, attacks):
-    answer = 0
     t, x, y = bandage
     max_health = health
     cnt, sequence = 1, 0
-    attacks.sort(key = lambda x: -x[0])
-    n_attack = attacks.pop()
-    last_attack = attacks[0][0]
+    attack_idx = 0
     
-    while True:
+    while attack_idx < len(attacks):
+        n_attack = attacks[attack_idx]
+        
         # 공격 시간인 경우
         if n_attack[0] == cnt:
             health -= n_attack[1]
             sequence = 0
+            # 체력이 0보다 작아진 경우
             if health <= 0:
                 return -1
-            
-            if attacks == []:
-                break
-            n_attack = attacks.pop()
+            attack_idx += 1
         
         # 공격 시간이 아닌 경우
         else:
             health += x
             sequence += 1
             
+            # 연속 성공일 경우
             if sequence == t:
                 health += y
                 sequence = 0
-                
+            
+            # 최대 체력 이상을 가지려고 할 때
             if health > max_health:
                 health = max_health
         
